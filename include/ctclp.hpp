835b55b3ea6e333687798f78925f1c9ed72c3686
Adam Czapla <adam@adamczapla.de>
2025-03-02 13:25:44 +0100

    add option class

--- include/ctclp.hpp
+++ include/ctclp.hpp
@@ -31,6 +31,28 @@ struct literal_string {
 
 };
 
+struct option {
+
+  enum type : size_t { integral, floating_point, string };
+  
+  friend auto operator<<(std::ostream& os, type const& opt_type) -> std::ostream& {
+      constexpr std::array<std::string_view, 2> type_messages {"digit", "string"};
+      auto const num = static_cast<int>(opt_type);
+      return os << "Option type: " << "(" << type_messages[num] << ")";
+  }
+  
+  constexpr option() = default;
+  constexpr option(std::string_view name, type value, std::string_view defval, std::string_view regex)
+      : name{name.data()}, value{value}, defval{defval.data()}, regex{regex.data()} 
+  {}
+
+  char const* name{};
+  type value{};
+  char const* defval{};
+  char const* regex{};
+
+};
+
 
 } // ctclp
 
