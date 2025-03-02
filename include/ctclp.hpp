7503095563b305c1203be45782752281b796f9e5
Adam Czapla <adam@adamczapla.de>
2025-03-02 13:23:54 +0100

    add literal_string

--- include/ctclp.hpp
+++ include/ctclp.hpp
@@ -1,2 +1,36 @@
 #pragma once
 
+#include <ctre.hpp>
+#include <ranges>
+
+namespace ctclp {
+
+namespace rng = std::ranges;
+using ctll::fixed_string;
+
+template <size_t size>
+struct literal_string {
+
+  constexpr literal_string(char const (&str)[size]) noexcept {
+    rng::copy_n(str, size, rng::begin(data)); 
+  }
+
+  constexpr auto to_string() const noexcept {
+    return std::string{rng::begin(data), std::prev(rng::end(data))};
+  }
+
+  constexpr auto to_string_view() const noexcept {
+    return std::string_view{data.data(), size};
+  }
+
+  constexpr auto to_view_remove_suffix() const noexcept {
+    return std::string_view{data.data(), size-1};
+  }
+
+  std::array<char, size> data{};
+
+};
+
+
+} // ctclp
+
